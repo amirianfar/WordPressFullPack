5f04ed1b7cd5070317a7380ae2b6235d76ede866
mILAD <93639222+amirianfar@users.noreply.github.com>
2023-09-23 12:47:27 +0330

    Adding Multiple node support

--- bash.sh
+++ bash.sh
@@ -8,59 +8,63 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-#nodePort=$1 
-nodeIP=$1
-hostSudoUser=$2
-#IPAndPort="$nodeIP:$nodePort"
-# echo $IPAndPort
-
-echo -e "\n${RED} ** This scripte has provided by Milad Amiriafar (amirianfar@gmail.com) \n for one-click installation a word press on kubernetes\n"
-
-sleep 1
-echo -e "\n** Node IP: $nodeIP"
-echo " ** Node Port: $nodePort"
-echo -e "  ** Host Sudo USER: $hostSudoUser ${NC}\n" 
+echo -e "\n${RED} ** This scripte has written by Milad Amiriafar (amirianfar@gmail.com) \n for one-click installation a word press on kubernetes\n"
 
 
-# login to remote host
-# ssh -p $nodePort -o StrictHostKeyChecking=no  $hostSudoUser@$nodeIP
+read -p "How many Worker Node do you have (Max: 5)? " WorkerNodeNo
+echo "Your cluster will have $WorkerNodeNo worker node(s)"
 
-ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/k8s_rsa -C "my k8s cluster key"
-ssh-copy-id -i $HOME/.ssh/k8s_rsa.pub $hostSudoUser@$nodeIP
+if ! [[ "$WorkerNodeNo" =~ ^[0-5]$ ]]; then
+  echo "Invalid input. Please enter an integer number between 0 and 5."
+  exit 1
+fi
 
-sleep 1
-echo -e "\n${RED} ** Updating package indexes ${NC}\n"
-sudo apt update
+# Run a for loop according to the parameter
+for (( i=0; i<$WorkerNodeNo; i++ )); do
+        read -p "Enter the IP address of worker node number $i: "  WorkerNodeIPAddress
+        read -p "Eneter the Sudoer user name of worker node number $i: "  WorkerNodeSudoer
+        echo -e "The IP address of worker node number $1 would be $WorkerNodeIPAddress and the Sudoer user name would be $WorkerNodeSudoer\n"
+        
+        # login to remote host
+        # ssh -p $nodePort -o StrictHostKeyChecking=no  $hostSudoUser@$nodeIP
 
-sleep 1
-echo -e "\n${RED} ** Installing  Git ${NC}\n"
-sudo apt install git
+        ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/k8s_rsa -C "my k8s cluster key"
+        ssh-copy-id -i $HOME/.ssh/k8s_rsa.pub $hostSudoUser@$nodeIP
 
-sleep 1
-echo -e "\n${RED} ** Cloning Kubespray repository ${NC}\n"
-git clone https://github.com/kubernetes-sigs/kubespray.git
+        sleep 1
+        echo -e "\n${RED} ** Updating package indexes ${NC}\n"
+        sudo apt update
 
-sleep 1 
-echo -e "\n${RED} ** Installing Python and Ansible Requirements ${NC}\n"
-cd kubespray 
-sudo apt-get install python3-venv
-sudo python3 -m venv venv
+        sleep 1
+        echo -e "\n${RED} ** Installing  Git ${NC}\n"
+        sudo apt install git
 
-echo -e "\n${RED} ** Activating the environment ${NC}\n"
-source venv/bin/activate
-sudo pip install -r requirements.txt
+        sleep 1
+        echo -e "\n${RED} ** Cloning Kubespray repository ${NC}\n"
+        git clone https://github.com/kubernetes-sigs/kubespray.git
 
-sudo modprobe nf_conntrack
-lsmod | grep nf_conntrack
-# #Ignore it for this version
-#sudo declare -r CLUSTER_FOLDER='my-cluster' 
-# # sudo cp -rfp inventory/sample inventory/$CLUSTER_FOLDER
+        sleep 1 
+        echo -e "\n${RED} ** Installing Python and Ansible Requirements ${NC}\n"
+        cd kubespray 
+        sudo apt-get install python3-venv
+        sudo python3 -m venv venv
 
-sudo cp -rfp inventory/sample inventory/milad-cluster
+        echo -e "\n${RED} ** Activating the environment ${NC}\n"
+        source venv/bin/activate
+        sudo pip install -r requirements.txt
 
-#sudo declare -a IPS=("$nodeIP")
-sudo CONFIG_FILE=inventory/milad-cluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${nodeIP[@]}
+        sudo modprobe nf_conntrack
+        lsmod | grep nf_conntrack
+        # #Ignore it for this version
+        #sudo declare -r CLUSTER_FOLDER='my-cluster' 
+        # # sudo cp -rfp inventory/sample inventory/$CLUSTER_FOLDER
 
-echo -e "\n${RED} ** Installing Kubespray... It can take a few minutes ...wait please... ${NC}\n"
-sleep 1
-ansible-playbook -vvv -i inventory/milad-cluster/hosts.yaml -u $hostSudoUser -b -v --private-key=$HOME/.ssh/k8s_rsa.pub cluster.yml -kK
+        sudo cp -rfp inventory/sample inventory/milad-cluster
+
+        #sudo declare -a IPS=("$nodeIP")
+        sudo CONFIG_FILE=inventory/milad-cluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${nodeIP[@]}
+
+        echo -e "\n${RED} ** Installing Kubespray... It can take a few minutes ...wait please... ${NC}\n"
+        sleep 1
+        ansible-playbook -vvv -i inventory/milad-cluster/hosts.yaml -u $hostSudoUser -b -v --private-key=$HOME/.ssh/k8s_rsa.pub cluster.yml -kK
+done 
